88b327d205310e5052dd633c43615a2adcf7e82c
JuanCarlos <juan.carlos.marques.pereira@gmail.com>
2026-01-25 21:54:18 -0300

    FEAT: add specialized input methods and robust error handling in Console class

--- src/com/juan/Console.java
+++ src/com/juan/Console.java
@@ -1,21 +1,47 @@
 package com.juan;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Locale;
 
-
 public class Console {
-    private static Scanner scanner = new Scanner(System.in);
-    public static double readNumber(String prompt, double min, double max) {
+    private static Scanner scanner;
+
+    static {
+        scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
-        double value;
+    }
+
+    public static double readDouble(String prompt, double min, double max) {
+        double value = 0;
         while (true) {
-            System.out.println(prompt);
-            value = scanner.nextDouble();
-            if (value >= min && value <= max) break;
-            System.out.println("Enter a value between " + min + " and " + max);
+            try {
+                System.out.println(prompt);
+                value = scanner.nextDouble();
+                if (value >= min && value <= max) break;
+                System.out.println("Enter a value between " + min + " and " + max);
+            } catch (InputMismatchException e) {
+                System.out.println("You need to enter a value between " + min + " and " + max);
+                scanner.nextLine();
+            }
         }
         return value;
+    }
 
+    public static int readInt(String prompt, int min, int max) {
+        int value = 0;
+        while (true) {
+            try {
+                System.out.println(prompt);
+                value = scanner.nextInt();
+                if (value >= min && value <= max) break;
+                System.out.println("Enter a value between " + min + " and " + max);
+            } catch (InputMismatchException e) {
+                System.out.println("You need to enter a decimal value between " + min + " and " + max);
+                scanner.nextLine();
+            }
+
+        }
+        return value;
     }
 }
--- src/com/juan/Main.java
+++ src/com/juan/Main.java
@@ -4,9 +4,9 @@ import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-        double principal = Console.readNumber("Principal (1,000 - 1,000,000): ", 1000, 1000000);
-        double annualInterest = Console.readNumber("Annual Interest Rate (1 - 30): ", 1, 30);
-        int years = (int) Console.readNumber("Period (Years 1 - 30): ", 1, 30);
+        double principal = Console.readDouble("Principal (1,000 - 1,000,000): ", 1000, 1000000);
+        double annualInterest = Console.readDouble("Annual Interest Rate (1 - 30): ", 1, 30);
+        int years = Console.readInt("Period (Years 1 - 30): ", 1, 30);
 
         Mortgage newMortgage = new Mortgage(principal, annualInterest, years);
         double result = newMortgage.calculate();
